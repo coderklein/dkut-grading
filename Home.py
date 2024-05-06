d614669d6b4e5b38ca7715cf0af1d118b6a8f98f
coderklein <contactqwishi@gmail.com>
2024-05-06 14:24:54 +0300

    changed favicon

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # STREAMLIT PAGE CONFIG.
 
-st.set_page_config(page_title='kimathi-grading', page_icon='🎯', layout="centered", initial_sidebar_state="collapsed")
+st.set_page_config(page_title='kimathi-grading', page_icon='🎓', layout="centered", initial_sidebar_state="collapsed")
 
 hide_streamlit_style = """ 
             <style>
